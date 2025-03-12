cbd6fb3045790f11c30b5ebed08a41aaaf5e32ef
ZZOMING-K <89764752+ZZOMING-K@users.noreply.github.com>
2025-03-12 16:57:07 +0900

    modify

--- train.py
+++ train.py
@@ -1,35 +1,33 @@
 from dataclasses import dataclass, field
 from typing import Optional
 import pandas as pd 
-from transformers import HfArgumentParser, set_seed
 from trl import SFTConfig, SFTTrainer
-from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments , BitsAndBytesConfig 
-from utils import create_datasets 
+from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments , BitsAndBytesConfig , HfArgumentParser, set_seed
+from utils import create_dataset
 from peft import LoraConfig
 
 # Define and parse arguments.
 @dataclass
 class ModelArguments:
-    model_name_or_path: str = field(default="rtzr/ko-gemma-2-9b-it")
-    lora_alpha: Optional[int] = field(default=16)
-    lora_dropout: Optional[float] = field(default=0.1)
-    lora_r: Optional[int] = field(default=8)
-    lora_target_modules: Optional[str] = field(default="q_proj,k_proj,v_proj,o_proj,down_proj,up_proj,gate_proj")
+    model_name_or_path: str = field(default="google/gemma-2-9b-it")
+    lora_r: Optional[int] = field(default=16)
+    lora_alpha: Optional[int] = field(default=32)
+    lora_dropout: Optional[float] = field(default=0.05)
+    lora_target_modules: Optional[list[str]] = field(default_factory=lambda: ["q_proj", "o_proj", "k_proj", "v_proj", "gate_proj", "up_proj", "down_proj"])
     lora_task_type: Optional[str] = field(default="CAUSAL_LM")
-    use_double_quant: Optional[bool] = field(default=False)
-    bnb_4bit_compute_dtype: Optional[str] = field(default="float16")
+    use_double_quant: Optional[bool] = field(default=True)
+    bnb_4bit_compute_dtype: Optional[str] = field(default="bfloat16")
     bnb_4bit_quant_type: Optional[str] = field(default="nf4")
     use_flash_attn: Optional[bool] = field(default=False)
-    use_peft_lora: Optional[bool] = field(default=False)
     use_8bit_quantization: Optional[bool] = field(default=False)
-    use_4bit_quantization: Optional[bool] = field(default=False) 
-    compute_dtype: Optional[str] = field(default="float16")
+    use_4bit_quantization: Optional[bool] = field(default=True)
+    compute_dtype: Optional[str] = field(default="bfloat16")
 
 @dataclass
 class TrainingArguments:
     output_dir: str = field(default="./results/models")
     per_device_train_batch_size: int = field(default=1)
-    per_device_eval_batch_size: int = field(defaust=1)
+    per_device_eval_batch_size: int = field(default=1)
     gradient_accumulation_steps: int = field(default=8)
     learning_rate: float = field(default=2e-5)
     max_steps: int = field(default=-1)
@@ -37,26 +35,27 @@ class TrainingArguments:
     logging_steps: int = field(default=10)
     logging_strategy: str = field(default="steps")
     save_strategy: str = field(default="steps")
-    save_total_limit: int = field(default=2)
+    eval_strategy : str = field(default="steps")
+    save_total_limit: int = field(default=3)
     eval_steps: int = field(default=100)
     lr_scheduler_type: str = field(default="linear")
-    fp16: bool = field(default=True)  
-    bf16: bool = field(default=False)
+    fp16: bool = field(default=False)
+    bf16: bool = field(default=True)
     seed: int = field(default=42)
     load_best_model_at_end: bool = field(default=True)
     metric_for_best_model: Optional[str] = field(default="eval_loss")
     push_to_hub: bool = field(default=True)
-    report_to: Optional[str] = field(default="wandb") 
-    max_seq_length: int = field(default=512)
-    dataset_text_file: str = field(default = 'text')
-    optim: str = field(default = 'adamw')
+    report_to: Optional[str] = field(default="wandb")
+    dataset_text_field: str = field(default = 'text')
+    optim: str = field(default = 'paged_adamw_8bit')
+    
 
 
 def main(model_args, training_args):
     
-    # Set seed for reproducibility
+ # Set seed for reproducibility
     set_seed(training_args.seed)
-    
+
     # quantization
     if model_args.use_4bit_quantization :
         bnb_config = BitsAndBytesConfig(
@@ -73,46 +72,80 @@ def main(model_args, training_args):
         bnb_config = None
 
     # model
-    model = AutoModelForCausalLM( 
-        model_args.model_name_or_path , 
-        device_map = 'auto' , 
-        quantization_config = bnb_config , 
-        torch_type = model_args.compute_dtype
+    model =  AutoModelForCausalLM.from_pretrained(
+        model_args.model_name_or_path ,
+        device_map = 'auto' ,
+        quantization_config = bnb_config ,
+        torch_dtype = model_args.compute_dtype , 
+        attn_implementation='eager'
     )
-    
-    tokenizer = AutoTokenizer.from_pretrained(model)
-    
-    df = pd.read_csv('./data/train.csv')
-    
+
+    tokenizer = AutoTokenizer.from_pretrained(model_args.model_name_or_path)
+
+    df = pd.read_csv('./data/prepro_train.csv')
+
     # datasets
-    train_dataset, eval_dataset = create_datasets(
+    train_dataset, eval_dataset = create_dataset(
         df,
         tokenizer,
     )
-    
+
+    # lora
     peft_config = LoraConfig(
         r=model_args.lora_r,
         lora_alpha=model_args.lora_alpha,
-        dropout=model_args.lora_dropout,
+        lora_dropout=model_args.lora_dropout,
         target_modules=model_args.lora_target_modules,
-        task_type = model_args.lora_task_type,
+        task_type=model_args.lora_task_type,
+    )
+
+
+ # trainingconfig
+    trainingargs = SFTConfig(
+        output_dir=training_args.output_dir,
+        per_device_train_batch_size=training_args.per_device_train_batch_size,
+        per_device_eval_batch_size=training_args.per_device_eval_batch_size,
+        gradient_accumulation_steps=training_args.gradient_accumulation_steps,
+        learning_rate=training_args.learning_rate,
+        max_steps=training_args.max_steps,
+        num_train_epochs=training_args.num_train_epochs,
+        logging_steps=training_args.logging_steps,
+        logging_strategy=training_args.logging_strategy,
+        save_strategy=training_args.save_strategy,
+        eval_strategy=training_args.eval_strategy,
+        save_total_limit=training_args.save_total_limit,
+        eval_steps=training_args.eval_steps,
+        lr_scheduler_type=training_args.lr_scheduler_type,
+        fp16=training_args.fp16,
+        bf16=training_args.bf16,
+        seed=training_args.seed,
+        load_best_model_at_end=training_args.load_best_model_at_end,
+        metric_for_best_model=training_args.metric_for_best_model,
+        push_to_hub=training_args.push_to_hub,
+        report_to=training_args.report_to,
+        dataset_text_field=training_args.dataset_text_field,
+        optim=training_args.optim,
+        run_name=training_args.run_name
     )
 
     # trainer
     trainer = SFTTrainer(
         model=model,
         tokenizer=tokenizer,
-        model_args=SFTConfig(training_args),
+        args=trainingargs,
         train_dataset=train_dataset,
         eval_dataset=eval_dataset,
         peft_config=peft_config,
     )
-    
+
+    # train
     trainer.train()
-    trainer.save_model(training_args.output_dir) #모델 저장 
-    
+
+    # model save
+    trainer.save_model(training_args.output_dir)
+
     ADAPTER_MODEL = './results/lora_adapter'
-    
+
     trainer.model.save_pretrained(ADAPTER_MODEL)
     tokenizer.save_pretrained(ADAPTER_MODEL)
 
